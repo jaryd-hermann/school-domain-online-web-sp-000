a1adc21dad5c8e8676971aa3410b43f815181828
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-24 12:51:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,11 +8,12 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
-    @name = name
-    @grade = grade
-    @roster[grade] = []
-    @roster[grade] << name
-  end
+  def add_student(student_name, grade)
+    if @roster[grade]
+        @roster[grade] << student_name
+      else
+        @roster[grade] = [student_name]
+      end
+    end
 
 end
